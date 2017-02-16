aed9facf344b34f2897710c37fb45c1c7d654f76
glauberBitfour <grobis_mn@hotmail.com>
2017-02-16 21:21:01 -0200

    Initial commit'Inicio Curso Loja'

---
+++ BitFour.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitFour.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestBitFour
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
