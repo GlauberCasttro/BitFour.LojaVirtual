4219c258941a5fb13982fe06d2bde9a58de5ab71
glauberBitfour <grobis_mn@hotmail.com>
2017-02-17 19:52:57 -0200

    Classe produto Adcionada

---
+++ BitFour.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitFour.LojaVirtual.Dominio.Entidades
+{
+    class Produto
+    {
+        public int produtoId { get; set; }
+        public string NomeProduto { get; set; }
+        public string Descricao { get; set; }
+        public decimal preco { get; set; }
+        public string categoria { get; set; }
+    }
+}
