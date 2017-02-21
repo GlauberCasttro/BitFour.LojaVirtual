bab572f54afed5d3298dd46da44f92d50959d83a
glauberBitfour <grobis_mn@hotmail.com>
2017-02-21 18:50:35 -0300

    Classe de paginaçao

---
+++ BitFour.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BitFour.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; }
+        public int ItensPorPagina { get; set; }
+        public int PaginaAtual { get; set; }
+        public int TotalPaginas
+        {
+
+            get
+            {
+                //MAth.ceiling arredonda o calculo pra cima se der um numero quebrado , para dar a quantidade de paginas correto 
+                return (int)Math.Ceiling((decimal)ItensTotal/ItensPorPagina);
+            }
+        }
+    }
+}
